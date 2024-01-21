5b5f9ce17b50a1d8000d6647566a27ad369d814e
WarriorX55 <141424663+WarriorX55@users.noreply.github.com>
2024-01-21 18:32:09 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+	import argparse
+	parser = argparse.ArgumentParser(description='[*] IP Geolocation Tool')
+	parser.add_argument('-u', '--url', help='Locate an IP based on a URL', action='store', default=False, dest='url')
+	parser.add_argument('-t', '--target', help='Locate the specified IP', action='store', default=False, dest='ip')
+	parser.add_argument('-f', '--dat', help='Custom database filepath', action='store', default=False, dest='datfile')
+	args = parser.parse_args()
